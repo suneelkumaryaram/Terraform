13abd9020d36210f6de127fea718df4a5071dea7
suneelkumaryaram <syaram@vaudoise.ch>
2025-03-06 16:56:46 +0100

    monolithic is on the way

---
+++ module-monolithic/dev/main.tf
@@ -0,0 +1,7 @@
+module "local-file" {
+  source   = "./../modules/local-file"
+  count    = 3
+  //how to add current folder name to prefix
+  filename = "${basename(path.module)}-hello-world-${count.index + 1}.txt"
+  content  = "hello world ${count.index + 1}"
+}
