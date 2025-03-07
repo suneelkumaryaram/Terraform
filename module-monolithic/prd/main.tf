0bcab2b0fbfed8a97e7b55554ecb38b1f8ec2df2
suneelkumaryaram <syaram@vaudoise.ch>
2025-03-07 06:57:56 +0100

    monolithic is done

--- module-monolithic/prd/main.tf
+++ module-monolithic/prd/main.tf
@@ -3,6 +3,6 @@ module "local-file" {
   count    = 3
   //how to add current folder name to prefix
   //filename = "${basename(path.module)}-hello-world-${count.index + 1}.txt"
-  filename = "dev-hello-world-${count.index + 1}.txt"
+  filename = "prd-hello-world-${count.index + 1}.txt"
   content  = "hello world ${count.index + 1}"
 }
